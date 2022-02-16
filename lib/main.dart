f09119b9143433421e897f42d3df9ede0ac17df7
YonathanZetune <yonathan@zetune.com>
2022-02-15 20:46:56 -0600

    fixed # routing

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,14 @@ import 'package:yz_personal_website/profile_page.dart';
 import 'package:flutter/material.dart';
 import 'package:yz_personal_website/github.dart';
 
-void main() => runApp(MyApp());
+import 'package:url_strategy/url_strategy.dart';
+
+void main() {
+  // Here we set the URL strategy for our web app.
+  // It is safe to call this function when running on mobile or desktop as well.
+  setPathUrlStrategy();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -18,10 +25,9 @@ class MyApp extends StatelessWidget {
 //        '/gSearch': (context) => new Search(),
       },
       theme: ThemeData(
-        brightness: Brightness.dark,
-        primaryColor: Colors.black,
-        fontFamily: "GoogleSansRegular"
-      ),
+          brightness: Brightness.dark,
+          primaryColor: Colors.black,
+          fontFamily: "GoogleSansRegular"),
       home: Stack(children: [ProfilePage()]),
     );
   }
